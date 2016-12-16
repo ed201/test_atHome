1c3855f1919842cbd66ff6c40cfe55b69033476a
ed <ed.lutt@gmail.com>
2016-12-16 10:15:03 +0100

    api : REST api (php7, symfony, api-platform)

---
+++ api/src/AppBundle/Entity/User.php
@@ -0,0 +1,135 @@
+<?php
+
+// src/AppBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * A user.
+ *
+ * @ApiResource
+ * @ORM\Entity
+ */
+class User
+{
+    /**
+     * @var int The id of this user.
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string The login of this user.
+     *
+     * @ORM\Column(type="text")
+     * @Assert\NotBlank
+     */
+    private $login;
+
+    /**
+     * @var string The password of this user.
+     *
+     * @ORM\Column(type="text")
+     * @Assert\NotBlank
+     */
+    private $passwd;
+
+    /**
+     * @var string The detail of this user.
+     *
+     * @ORM\Column
+     * @Assert\NotBlank
+     */
+    private $detail;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set login
+     *
+     * @param string $login
+     *
+     * @return User
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    /**
+     * Get login
+     *
+     * @return string
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * Set passwd
+     *
+     * @param string $passwd
+     *
+     * @return User
+     */
+    public function setPasswd($passwd)
+    {
+        $this->passwd = $passwd;
+
+        return $this;
+    }
+
+    /**
+     * Get passwd
+     *
+     * @return string
+     */
+    public function getPasswd()
+    {
+        return $this->passwd;
+    }
+
+    /**
+     * Set detail
+     *
+     * @param string $detail
+     *
+     * @return User
+     */
+    public function setDetail($detail)
+    {
+        $this->detail = $detail;
+
+        return $this;
+    }
+
+    /**
+     * Get detail
+     *
+     * @return string
+     */
+    public function getDetail()
+    {
+        return $this->detail;
+    }
+    
+}
